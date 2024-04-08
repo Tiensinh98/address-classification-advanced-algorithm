40e45344e39607b90910800f00073e0b5631fa98
Tiensinh98 <phamtiensinh010@gmail.com>
2024-04-09 03:54:06 +0700

    some updates to bypass the full existing names, and re-correct input data

--- classifier.py
+++ classifier.py
@@ -4,6 +4,7 @@ import typing as tp
 import numpy as np
 
 HCM_SPECIAL_NUMBERS = [str(i) for i in range(1, 20)]
+HCM = "hồchíminh"
 
 
 class TrieNode:
@@ -34,43 +35,55 @@ class Trie:
             temp_root = temp_root.children[node_name]
 
     def search(self, node_names: list,
-               parent_node: tp.Union[TrieNode, None] = None) -> tp.Union[TrieNode, None]:
+               parent_node: tp.Union[TrieNode, None] = None, use_unidecode=False) -> tp.Union[TrieNode, None]:
         if parent_node is None:
             parent_node = self.root
         found_node = None
         for node_name in node_names:
             found_node = parent_node.children.get(node_name, None)
             if found_node is None:
-                return None
-            parent_node = parent_node.children[node_name]
+                if use_unidecode:
+                    # case (hóa and hoá)
+                    node_name_without_diacritics = unidecode.unidecode(node_name)
+                    found_node = {
+                        unidecode.unidecode(name): node
+                        for name, node in parent_node.children.items()
+                    }.get(node_name_without_diacritics, None)
+                    if found_node is None:
+                        return None
+                    parent_node = parent_node.children[found_node.node_name]
+                else:
+                    return None
+            else:
+                parent_node = parent_node.children[node_name]
         return found_node
 
 
 class Solution:
     def __init__(self):
         # build trie
+        self.phrase_trie = None
         self.word_trie = None
         self.letter_trie = None
-        self.build_trie('dataset/dataset.csv')
+        self.build_trie('dataset/new_dataset.csv')
 
     def build_trie(self, data_filepath: str):
         with open(data_filepath, mode='r', encoding='utf-8') as file:
-            self.word_trie = Trie()
+            self.phrase_trie = Trie()
             self.letter_trie = Trie()
             csv_file = csv.reader(file)
             for line in csv_file:
-                self.word_trie.insert([
+                self.phrase_trie.insert([
                     [line[0], line[3]],
                     [line[1], line[4]],
                     [line[2], line[5]]
                 ])
-                for tup in [(0, 3), (1, 4), (2, 5)]:
-                    letters0 = list(line[tup[0]])
-                    # letters1 = list((line[tup[1]].replace(' ', '')))
-                    pairs = np.array([letters0[::-1], letters0[::-1]]).T.tolist()
+                for index in range(3):
+                    letters = list(unidecode.unidecode(line[index]))
+                    pairs = np.array([letters[::-1], letters[::-1]]).T.tolist()
                     self.letter_trie.insert(pairs)
 
-    def find_trie_node(self, sticky_address, parent_node=None) -> tp.Tuple[TrieNode, str]:
+    def find_trie_node(self, sticky_address, parent_node=None, use_unidecode=False) -> tp.Tuple[TrieNode, str]:
         current_word = ''
         index = -1
         shift = 0
@@ -79,20 +92,22 @@ class Solution:
         temp_found_word = None
         while not found and shift - index <= len(sticky_address):
             current_word += sticky_address[index - shift]
-            found_letter = self.letter_trie.search(list(current_word))
+            found_letter = self.letter_trie.search(list(unidecode.unidecode(current_word)))
             index -= 1
             if found_letter is None:
                 if temp_found_word is not None:
                     found_node = temp_found_word
                     found = True
                     index += 1
-                current_word = ''
+                current_word = ''  # should we just trim the latest letter that makes the string not found?
                 shift += 1
                 index = -1
             else:
-                # TODO: check case overlapping numbers found i.e district 3 found but actual the correct answer is 13
-                found_word = self.word_trie.search([current_word[::-1]], parent_node=parent_node)
+                found_word = self.phrase_trie.search(
+                    [current_word[::-1]], parent_node=parent_node, use_unidecode=use_unidecode)
                 if current_word[::-1] in HCM_SPECIAL_NUMBERS and found_word is not None:
+                    # check case overlapping numbers found i.e.
+                    # district 3 found but actual the correct answer is 13
                     if len(current_word) == 1:
                         temp_found_word = found_word
                         continue
@@ -113,16 +128,14 @@ class Solution:
 
     def process(self, s: str):
         # Remove diacritics using unidecode
-        address_without_diacritics = unidecode.unidecode(s)
-        sticky_address = (address_without_diacritics
-                          .replace(',', '')
+        sticky_address = (s.replace(',', '')
                           .replace(' ', '')
                           .replace('.', '')
                           .replace("'", '')
                           .replace("-", '').lower())
-        province_node, sticky_address = self.find_trie_node(sticky_address)
+        province_node, sticky_address = self.find_trie_node(sticky_address, use_unidecode=True)
         if province_node is None:
-            province_nodes = self.word_trie.root.children.values()
+            province_nodes = self.phrase_trie.root.children.values()
             possible_district_node_to_sticky_address = []
             for node in province_nodes:
                 # TODO: check match letter cases above is subset of node.node_name
@@ -135,34 +148,50 @@ class Solution:
             if len(possible_district_node_to_sticky_address) == 1:
                 district_node, province_node, sticky_address = possible_district_node_to_sticky_address[0]
             elif len(possible_district_node_to_sticky_address) == 2:
-                second_district_node = possible_district_node_to_sticky_address[1][0]
-                second_province_node = possible_district_node_to_sticky_address[1][1]
+                second_district_node, second_province_node, _ = possible_district_node_to_sticky_address[1]
                 if (second_district_node.node_name in HCM_SPECIAL_NUMBERS
-                        and second_province_node.node_name == 'hochiminh'):
-                    district_node = possible_district_node_to_sticky_address[0][0]
-                    province_node = possible_district_node_to_sticky_address[0][1]
+                        and second_province_node.node_name == HCM):
+                    district_node, province_node, _ = possible_district_node_to_sticky_address[0]
                 else:
                     district_node = None
             else:
                 district_node = None
         else:
+            before_sticky_address = sticky_address
             district_node, sticky_address = self.find_trie_node(
-                sticky_address, parent_node=province_node)
+                sticky_address, parent_node=province_node, use_unidecode=True)
+            if district_node is None:
+                sticky_address = before_sticky_address
         if district_node is not None:
-            ward_node, _ = self.find_trie_node(sticky_address, parent_node=district_node)
+            ward_node, _ = self.find_trie_node(
+                sticky_address, parent_node=district_node, use_unidecode=True)
         else:
             if province_node is None:
+                # retrieve district/province from reverse trie?
                 ward_node = None
             else:
-                district_nodes = self.word_trie.root.children[
+                district_nodes = self.phrase_trie.root.children[
                     province_node.node_name].children.values()
                 possible_ward_nodes = []
                 for node in district_nodes:
-                    ward_node, _ = self.find_trie_node(sticky_address, parent_node=node)
+                    ward_node, _ = self.find_trie_node(sticky_address, parent_node=node, use_unidecode=True)
                     if ward_node is not None:
-                        possible_ward_nodes.append(ward_node)
+                        possible_ward_nodes.append((ward_node, node))
                 if len(possible_ward_nodes) == 1:
-                    ward_node = possible_ward_nodes[0]
+                    ward_node, district_node = possible_ward_nodes[0]
+                    if len(s1 := s.split(',')) >= 2 and not len(s1[-2].replace(' ', '')):
+                        # suppose not length of district in input address, so maybe it doesn't have district
+                        district_node = None
+                elif len(possible_ward_nodes) == 2:
+                    second_ward_node, second_district_node = possible_ward_nodes[1]
+                    if second_ward_node.node_name in HCM_SPECIAL_NUMBERS:
+                        ward_node, district_node = possible_ward_nodes[0]
+                    # else:
+                    #     ward_node = None
+                    # suppose not length of district in input address, so maybe it doesn't have district
+                    if len(s1 := s.split(',')) >= 2 and not len(s1[-2].replace(' ', '')):
+                        # suppose not length of district in input address, so maybe it doesn't have district
+                        district_node = None
                 else:
                     ward_node = None
         #
@@ -184,6 +213,6 @@ class Solution:
 
 
 if __name__ == '__main__':
-    input = "L10C12 Hẻm 90, Ng, Bỉnh Khiêm, TP. Rạch Giá, Kiên Giang"
+    input = "Thái Ha, HBa Vì, T.pHNội"
     solution = Solution()
     solution.process(input)
--- test_public_address.py
+++ test_public_address.py
@@ -12,29 +12,26 @@ def solution():
 
 
 @pytest.mark.parametrize('case_file_path', [
-    'test_cases/full_address_test_cases.json',
+    'test_cases/basic_cases.json',
     'test_cases/confusing_number_hcm_cases.json',
-    'test_cases/inconsistent_information.json'
+    'test_cases/inconsistent_information.json',
+    'test_cases/other_cases.json'
 ])
 def test_full_address_cases(solution, case_file_path):
     test_cases = json.load(open(case_file_path, encoding='utf-8'))
     count = 0
     timer = []
-    print('Reading test case: ', case_file_path)
+    print('Reading test case: ', case_file_path, '\n')
     for test_case in test_cases:
         start = time.time()
         output = solution.process(test_case['text'])
         time_elapsed = time.time() - start
         timer.append(time_elapsed)
-        try:
-            assert time_elapsed < 0.015
-            expected_result = test_case['result']
-            assert expected_result['province'] == output['province']
-            assert expected_result['district'] == output['district']
-            assert expected_result['ward'] == output['ward']
-            count += 1
-        except AssertionError:
-            print('Failed test case: ', test_case)
-            continue
+        assert time_elapsed < 0.05
+        expected_result = test_case['result']
+        assert expected_result['province'] == output['province']
+        assert expected_result['district'] == output['district']
+        assert expected_result['ward'] == output['ward']
+        count += 1
     print(f'Passed: {count} cases / {len(test_cases)}')
     assert np.average(timer) < 4. / 100, 'Average time exceeds the allowed amount of time.'
